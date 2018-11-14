d0cd32fb03a589c08b603f2555cd7af9b4b9ca45
Maris Valentine <valentine.maris@student-cs.com>
2018-11-14 16:56:38 +0100

    ajout

--- twitter_collect/search.py
+++ twitter_collect/search.py
@@ -3,8 +3,10 @@ from twitter_collect import twitter_connection_setup
 def collect(query):
     api= twitter_connection_setup.twitter_setup()
     tweets=api.search(query,rpp=5)  #rpp max 100
+    liste_tweet=[]
     for tweet in tweets:
-        print(tweet.text)
+        liste_tweet.append(tweet.text)
+    return liste_tweet
 
 collect("@Emmanuelmacron")
 
