1b477d29765387bcc83a1876897869eca01d34ee
Maris Valentine <valentine.maris@student-cs.com>
2018-11-15 00:17:51 +0100

    test

--- twitter_collect/collect_candidate_tweet_activity.py
+++ twitter_collect/collect_candidate_tweet_activity.py
@@ -1,16 +1,30 @@
-from twitter_collect import
+from twitter_collect import user
+from twitter_collect import twitter_connection_setup
+api= twitter_connection_setup.twitter_setup()
 
-def get_candidate_queries(num_candidate, file_path):
-    """
-    Generate and return a list of string queries for the search Twitter API from the file file_path_num_candidate.txt
-    :param num_candidate: the number of the candidate
-    :param file_path: the path to the keyword and hashtag
-    files
-    :param type: type of the keyword, either "keywords" or "hashtags"
-    :return: (list) a list of string queries that can be done to the search API independently
-    """
-    try:
-        # TO COMPLETE
-    except IOError:
-        # TO COMPLETE
+#dico contient
+def get_retweets_of_candidate(num_candidate):
+    screen_name_candidat = recuperer_candidate(num_candidate)  #verif screen
+    tweets=api.user_timeline(screen_name=screen_name_candidat, count=200)
+    for tweet in tweets:
+        if retweeted:
+            retweets=api.retweets(tweet.id)
+    return retweets
+
+
+
+def recuperer_candidate(num_candidate):
+    path_keyword= "C:/Users/valen/Documents/programmation/twitterPredictor/CandidateData/keywords_candidate_" + str(num_candidate) +".txt"
+    fichier_keyword = open("path_keyword","r")
+    contenu_keyword=fichier_keyword.read()
+    keyword= contenu_keyword.split(" ")
+    return keyword[0] + keyword[1]                                       #le premier mot de chquaque fichier est le prenom puis nom
+
+
+
+
+
+public_tweets = api.search("stackoverflow")
+for tweet in public_tweets:
+    print api.retweets(tweet.id)
 
