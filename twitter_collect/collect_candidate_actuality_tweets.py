8f0f89afe9bc206eaef1e9643fdcc9005ff17ede
Maris Valentine <valentine.maris@student-cs.com>
2018-11-14 21:58:11 +0100

    test

--- twitter_collect/collect_candidate_actuality_tweets.py
+++ twitter_collect/collect_candidate_actuality_tweets.py
@@ -3,10 +3,17 @@ from twitter_collect import twitter_connection_setup
 
 twitter_api=twitter_connection_setup.twitter_setup()
 
-def get_tweets_from_candidates_search_queries(queries,twitter_api):
-    tweets=[]
-    for q in queries:
-        tweets.append(search.collect(q))
-    return tweets
+""""étant donnée une liste queries de requêtes (de type search) et une instance de connexion twitter_api récupére et 
+renvoie les tweets répondant aux différentes requêtes."""
 
+def get_tweets_from_candidates_search_queries(queries,twitter_api):
+    tweets={}
+    try:
+        for q in queries:
+            tweets[q]=search.collect(q)
+        return tweets
+    except TweepError:
+		print(TweepError.response.text)
+	except RateLimitError:
+        print("RateLimitError")
 
