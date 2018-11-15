314e9dfc16c33845840a34f41fdc4b8373b02e9d
Maris Valentine <valentine.maris@student-cs.com>
2018-11-15 16:07:22 +0100

    première version

--- twitter_collect/storage.py
+++ twitter_collect/storage.py
@@ -4,27 +4,20 @@ import matplotlib.pyplot as plt
 from twitter_collect import twitter_connection_setup
 import json
 
-"""comment tranfo un dico python en object json
-Dump and load convert between files and objects, while dumps and loads convert between strings and objects.
+#object Status: {Satutus...., _json: {....}}
 
-import json
-with open("data_file.json", "w") as write_file:
-    json.dump(data, write_file)                                     permet de créer un dossier data_file qui contient le json
-
-json_string = json.dumps(data)                                      permet de
-"""
-
+#etape 1,on recupere le json sous la forme d'un dico
 def collect_tweet(query):
     liste_tweet=[]
     connexion = twitter_connection_setup.twitter_setup()
     tweets = connexion.search(query,language="french",rpp=100)
     for tweet in tweets:
         liste_tweet.append(tweet._json)     #pour recuperer json
-    print(liste_tweet)
+    #print(liste_tweet)
     return liste_tweet
 
-tweet_test=collect_tweet("macron")[0]
-print(type(tweet_test))
+#tweet_test=collect_tweet("macron")[0]
+#print(type(tweet_test))
 
 
 #entrée: un tweet json, nom du fichier qui va être créée et où on va stocker le json
@@ -43,7 +36,7 @@ def lavage(tweets):
 
 
 #entrée: tweets est un dico qui possede les attribut d'un tweet
-def store_et_lavage_tweets(tweets):
+def store_et_lavage_tweets(tweets, filename):
     #tweet_dico=json.load(tweets)  pour convertir json en dico
     tweets_lavé=lavage(tweets)
     fichier=open(filename,"w")
@@ -51,4 +44,7 @@ def store_et_lavage_tweets(tweets):
     fichier.close()
 
 #store_tweets(tweet_test, "test.json")
-
+def dataframe(tweets):
+    tweet_propre=lavage(tweets)
+    Dataframe= pd.DataFrame(tweet_propre)
+    return Dataframe
