e3d6331609f6015dea815991cf23bc4eed7e8efc
Maris Valentine <valentine.maris@student-cs.com>
2018-11-15 09:20:31 +0100

    search

--- twitter_collect/search.py
+++ twitter_collect/search.py
@@ -1,12 +1,16 @@
 from twitter_collect import twitter_connection_setup
 
+#entrer : une requete
+#sortie: un tableau des texts des 100 derniers tweets ou apprarrait la requete, et les tweet avec toutes les info
 def collect(query):
     api= twitter_connection_setup.twitter_setup()
-    tweets=api.search(query,rpp=5)  #rpp max 100
+    tweets=api.search(query,rpp=100)  #rpp max 100
     liste_tweet=[]
+    tweets=[]
     for tweet in tweets:
         liste_tweet.append(tweet.text)
-    return liste_tweet
+        print(liste_tweet)
+    return liste_tweet, tweets
 
 collect("@Emmanuelmacron")
 
