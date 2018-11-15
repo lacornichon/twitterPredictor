27f2be9a00fe352f945d28207303bfde5ca90deb
Maris Valentine <valentine.maris@student-cs.com>
2018-11-15 09:22:59 +0100

    ajout com

--- twitter_collect/user.py
+++ twitter_collect/user.py
@@ -1,5 +1,7 @@
 from twitter_collect import twitter_connection_setup
 
+ #entrée: user_id, càd l'id d'un compte twiteer
+ #sortie:le tableau contenant tout les 200 dernier tweets de ce compte
 def user(user_id):
     api=twitter_connection_setup.twitter_setup()
     statuses = api.user_timeline(id=user_id, count=200)
