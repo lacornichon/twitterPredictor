682eb59f3bebd42da50c225248001aeb8a12aa9c
Maris Valentine <valentine.maris@student-cs.com>
2018-11-15 14:35:14 +0100

    programme deinitif

--- twitter_collect/tweet_collect_whole.py
+++ twitter_collect/tweet_collect_whole.py
@@ -38,22 +38,30 @@ print (get_candidate_(1,'CandidateData'))
 
 #entrée num-candidate, file_path
 #sortie : dico de clé:mot clé et associé aux 200 dernier tweets contenant mot clé
+#rappel, collect renvoi {status, _json}
 
 
 
 def get_candidate_queries(num_candidate, file_path):
-    tweets_keywords={}
-    tweets_hastag={}
-    queries_hastag=[]
-    queries_keywords=[]
-    queries_keywords=get_candidate_(num_candidate,file_path)[0]
-    queries_hastag=get_candidate_(num_candidate,file_path)[1]
-    for k in queries_keywords:
-       tweets_keywords[k]=collect(k)[1]
-    for h in queries_hastag:
-        tweets_hastag[h]=collect(h)[1]
-    print(tweets_hastag)
-    print(tweets_keywords)
-    return tweets_keywords, tweets_hastag
+    try:
+        tweets_keywords={}                   #dico avec toutes les infos
+        tweets_hastag={}
+        collected_keyword={}                 #dico avec info utiles
+        collected_hashtag={}
+        queries_hastag=[]                   #retour brut de collect sous la forme {status, _json}
+        queries_keywords=[]
+        queries_keywords=get_candidate_(num_candidate,file_path)[0]         #on séléctionne les keywords
+        queries_hastag=get_candidate_(num_candidate,file_path)[1]           #on séléctionne les hashtages
+        for k in queries_keywords:
+           tweets_keywords[k]=collect(k)[1]              #Extrait les tweets à l'aide de la fonction collect à partir des keywords du candidats choisi, on recupère un dic
+        for h in queries_hastag:
+            tweets_hastag[h]=collect(h)[1]              #Extrait les tweets à l'aide de la fonction collect à partir des hashtags candidat choiisi
+        for tweet in tweets_keywords:
+             collected_keyword[tweet.id]=[tweet.user.name,tweet.text,tweet.retweet_count,tweet.favorite_count]           #pour chaque tweet on recup info utiles
+        for hashtag in tweets_hastag:
+            collected_hashtag[tweet.id]=[tweet.user.name,tweet.text,tweet.retweet_count,tweet.favorite_count]
+        return tweets_keywords, tweets_hastag
+    except IOError as error:
+        print(error)
 
 print(get_candidate_queries(1,'CandidateData'))
