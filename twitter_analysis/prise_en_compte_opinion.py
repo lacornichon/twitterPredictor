e0d0c0875bc55ef45b4d60a186cda97b1bb78ae5
TimFaverjon <tim.faverjon@student-cs.fr>
2018-11-16 16:45:23 +0100

    creation d'un fonction pour l'opinion

--- twitter_analysis/prise_en_compte_opinion.py
+++ twitter_analysis/prise_en_compte_opinion.py
@@ -17,31 +17,25 @@ def collect(query):
         liste_tweet_text.append(tweet.text)
     return liste_tweet_text
 
-#pb de genre
-tweets=collect("macron")
-tweets_blob=TextBlob('.'.join(tweets))
-
-tweets_polarity=[]
-for sentences in tweets_blob.sentences:
-    tweets_polarity.append(sentences.sentiment.polarity)
-moyenne=sum(tweets_polarity)/len(tweets_polarity)
-
 """"on def, tweet positif pour polarité>0.3, négatifs pour polarité<-0.3"""
-positifs=[]
-negatifs=[]
-neutres=[]
-for sentiment in tweets_polarity:
-    if sentiment<-0.3:
-        negatifs.append(sentiment)
-    elif sentiment>0.3:
-        positifs.append(sentiment)
-    else:
-        neutres.append(sentiment)
+def collect_opinion(query):
+    #pb de genre
+    tweets=collect(query)
+    tweets_blob=TextBlob('.'.join(tweets))
 
-pourcentage_positif=len(positifs)/len(tweets_polarity)
-pourcentage_negatif=len(negatifs)/len(tweets_polarity)
-pourcentage_neutre=len(neutres)/len(tweets_polarity)
-
-print("Percentage of positive tweets: {}%".format(pourcentage_positif*100))
-print("Percentage of neutral tweets: {}%".format(pourcentage_negatif*100))
-print("Percentage de negative tweets: {}%".format(pourcentage_neutre*100))
+    tweets_polarity=[]
+    for sentences in tweets_blob.sentences:
+        tweets_polarity.append(sentences.sentiment.polarity)
+    moyenne=sum(tweets_polarity)/len(tweets_polarity)
+    pos,neg,neutre=0,0,0
+    for sentiment in tweets_polarity:
+        if sentiment<-0.3:
+            neg+=1
+        elif sentiment>0.3:
+            pos+=1
+        else:
+            neutre+=1
+    pourcentage_positif=pos/len(tweets_polarity)*100
+    pourcentage_negatif=neg/len(tweets_polarity)*100
+    pourcentage_neutre=neutre/len(tweets_polarity)*100
+    return([pourcentage_positif,pourcentage_negatif,pourcentage_neutre])
