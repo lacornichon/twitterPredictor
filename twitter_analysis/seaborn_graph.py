7157e20e21a5a0842c7e7c12345eb38797977ae2
TimFaverjon <tim.faverjon@student-cs.fr>
2018-11-16 15:15:37 +0100

    graph

--- twitter_analysis/seaborn_graph.py
+++ twitter_analysis/seaborn_graph.py
@@ -7,20 +7,14 @@ sns.set(style="darkgrid")
 
 # Load an example dataset with long-form data
 fmri = sns.load_dataset("fmri")
+print(fmri)
 
+#Macron vs Edouard-Philippe
+
+dataf=storage.dataframe("Macron")
+print(dataf)
 # Plot the responses for different events and regions
-sns.lineplot(x="timepoint", y="signal", hue="region", style="event", data=fmri)
+sns.lineplot(x="created_at", y="retweet_count", hue="about", style="about", data=dataf)
 
-query="Macron"
-
-data=storage.dataframe(query)
-retweet_c = pd.Series(data=data['retweet_count'].values, index=data['created_at'])
-retweet_c.plot(figsize=(16,4), label="Retweets", legend=True)
-
-query="Michel"
-
-data=storage.dataframe(query)
-retweet_c = pd.Series(data=data['retweet_count'].values, index=data['created_at'])
-retweet_c.plot(figsize=(16,4), label="Retweets", legend=True)
 
 plt.show()
