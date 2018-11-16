651fa7a7cdccf5f28c7a7a5ce7b9cded07535a83
TimFaverjon <tim.faverjon@student-cs.fr>
2018-11-16 17:03:41 +0100

    app avec commentaire omg

--- Dash_visual/app.py
+++ Dash_visual/app.py
@@ -4,12 +4,16 @@ import dash_core_components as dcc
 import dash_html_components as html
 from twitter_analysis import prise_en_compte_opinion
 
+"""
+ici on crée un tableau qui comparre tweet positif negatif et neutre pour les 2 mots en queries (test et tu comprendra)
+"""
+
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 """on def un object app avec un constructeur Dash, puis on lui aplliquera des methodes et on utlisiera ces propriétés"""
 
-queries=["Chocolatine","Pain au chocolat"]
+queries=["Homme","Femme"]
 
 app.layout = html.Div(children=[                                   #comme en html, balise Div et H1, imbriquées
     html.H1(children='Hello my name is winner'),
